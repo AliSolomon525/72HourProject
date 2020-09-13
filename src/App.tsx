a2b2dd35c3ba915df16f8d28cd4253914f9a20ac
unknown <laurenalayzacodes@gmail.com>
2020-09-13 18:41:24 -0400

    going to switchbranches nasa

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Nasa from "./Nasa";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Nasa />
+    </div>
+  );
 }
 
 export default App;
--- src/Nasa.tsx
+++ src/Nasa.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-export interface NASAProps {
-  url: string;
-}
-
 export interface NASAState {
   NASAInfo: Blob;
 }
 
-class NASA extends React.Component<NASAProps, NASAState> {
-  constructor(props: NASAProps) {
+class NASA extends React.Component<NASAState> {
+  constructor(props: any) {
     super(props);
     this.state = { NASAInfo: new Blob() };
   }
@@ -19,15 +15,20 @@ class NASA extends React.Component<NASAProps, NASAState> {
       `https://api.nasa.gov/planetary/earth/imagery?lon=-85.863661&lat=39.9426726&date=2014-02-01&api_key=bUkM9xqZO6ZKwYGx3g7IeGRg9Yt1WCKjcPjOKwGc`
     )
       .then((res) => res.json())
-      .then((json: Blob) => {
-        console.log(json);
-        this.setState({ NASAInfo: json });
+      .then((data: Blob) => {
+        console.log(data);
+        this.setState({ NASAInfo: data });
       });
   }
 
   render() {
     console.log("testing");
-    return <div>Testing NASAInfo</div>;
+
+    return (
+      <div>
+        <img src="{NASAInfo}" />
+      </div>
+    );
   }
 }
 
