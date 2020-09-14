3ff633d63a0e50fcdfeabd36ba570765b7b1d07b
unknown <cbauerband@ivytech.edu>
2020-09-13 22:36:00 -0400

    merged carissa to develop

--- src/Geolocation/Geolocation.tsx
+++ src/Geolocation/Geolocation.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
    
 export interface GeoState {
   longitude: any,
@@ -30,30 +29,3 @@ export interface GeoProps {
   }
    
   export default Geolocation;
-=======
-// import { render } from "react-dom";
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   componentDidMount() {
-//     navigator.geolocation.getCurrentPosition(function (position) {
-//       console.log("Latitude:", position.coords.latitude);
-//       console.log("Longitude:", position.coords.longitude);
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h4>geolocation</h4>
-//       </div>
-//     );
-//   }
-// }
-
-// render(<App />, document.getElementById("root"));
->>>>>>> f27260bf5c59d692d84c828d1dbac5f2e5026f00
