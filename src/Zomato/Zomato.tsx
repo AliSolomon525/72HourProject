55e3c5b5cdbe2541b27fbb51ee0a3b6b07abb94d
unknown <cbauerband@ivytech.edu>
2020-09-13 22:22:16 -0400

    got Zomato working!

--- src/Zomato/Zomato.tsx
+++ src/Zomato/Zomato.tsx
@@ -16,22 +16,25 @@ class Zomato extends React.Component<ZomatoProps,ZomatoState> {
             ZomatoResults: {}
         }
     }
-    componentDidMount() {
-        fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${this.props.latitude}&lon=${this.props.longitude}`, {
-            method: 'GET',
-            headers: {
-                'user-key': 'f0e967e1b7213e6511c3900caf3f8aac',
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then((json: ZomatoInterface) => {
-            console.log(json)
-            this.setState({ZomatoResults: json})
-        })
-    }
+    componentDidUpdate(prevProps: ZomatoProps) {
+        // Typical usage (don't forget to compare props):
+        if (this.props.latitude !== prevProps.latitude) {
+            fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${this.props.latitude}&lon=${this.props.longitude}`, {
+                method: 'GET',
+                headers: {
+                    'user-key': 'f0e967e1b7213e6511c3900caf3f8aac',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then((json: ZomatoInterface) => {
+                console.log(json)
+                this.setState({ZomatoResults: json})
+            })
+        }
+      }
     render() { 
-        return ( <div></div> );
+        return ( <div>{this.state.ZomatoResults.nearby_restaurants !== undefined ? this.state.ZomatoResults.nearby_restaurants[0].restaurant.name : <></>}</div> );
     }
 }
  
